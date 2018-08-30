0c0a27801c751890ec730ef2521414b92b48ae3d
ByronGBP <byrongbp@gmail.com>
2018-08-30 16:31:10 +0200

    carousel styles

--- src/components/Welcome/Carousel.js
+++ src/components/Welcome/Carousel.js
@@ -2,75 +2,52 @@
 import React from 'react';
 
 const Carousel = () => (
-  <div id="carouselExampleControls" className="w-100 nvx-h-100 carousel slide" data-ride="carousel" data-interval="0">
+  <div id="carouselExampleControls" className="w-100 nvx-h-100 carousel slide" data-ride="carousel" data-interval="5000">
     <div className="carousel-inner nvx-h-100">
 
       <div className="carousel-item nvx-h-100 active">
-        <div className="row nvx-h-100 justify-content-center">
-          <div className="col-8 nvx-h-100">
-            <div className="row justify-content-around nvx-h-100">
+        <div className="d-flex nvx-h-100 justify-content-center">
+          <div className="row px-3 justify-content-around nvx-h-100">
 
-              <div className="col-6 nvx-h-100 d-flex flex-column justify-content-center align-items-center">
-                <div className="nvx-thumb"></div>
-                <div className="nvx-thumb-text text-center">Tame impala</div>
-              </div>
-              <div className="col-6 nvx-h-100 d-flex flex-column justify-content-center align-items-center">
-                <div className="nvx-thumb"></div>
-                <div className="nvx-thumb-text text-center">Tame impala</div>
-              </div>
-
+            <div className="col-4 nvx-h-100 d-flex flex-column justify-content-center align-items-center">
+              <div className="nvx-thumb"></div>
+              <div className="nvx-thumb-text text-center"><span>Artist name</span></div>
             </div>
+            <div className="col-4 nvx-h-100 d-flex flex-column justify-content-center align-items-center">
+              <div className="nvx-thumb"></div>
+              <div className="nvx-thumb-text text-center">Artist name</div>
+            </div>
+            <div className="col-4 nvx-h-100 d-flex flex-column justify-content-center align-items-center">
+              <div className="nvx-thumb"></div>
+              <div className="nvx-thumb-text text-center">Artist name</div>
+            </div>
+
           </div>
         </div>
       </div>
 
       <div className="carousel-item nvx-h-100">
-        <div className="row nvx-h-100 justify-content-center">
-          <div className="col-8 nvx-h-100">
-            <div className="row justify-content-around nvx-h-100">
+        <div className="d-flex nvx-h-100 justify-content-center">
+          <div className="row px-3 justify-content-around nvx-h-100">
 
-              <div className="col-6 nvx-h-100 d-flex flex-column justify-content-center align-items-center">
-                <div className="nvx-thumb"></div>
-                <div className="nvx-thumb-text text-center">Tame impala</div>
-              </div>
-              <div className="col-6 nvx-h-100 d-flex flex-column justify-content-center align-items-center">
-                <div className="nvx-thumb"></div>
-                <div className="nvx-thumb-text text-center">Tame impala</div>
-              </div>
-
+            <div className="col-4 nvx-h-100 d-flex flex-column justify-content-center align-items-center">
+              <div className="nvx-thumb"></div>
+              <div className="nvx-thumb-text text-center"><span>Artist name</span></div>
             </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="carousel-item nvx-h-100">
-        <div className="row nvx-h-100 justify-content-center">
-          <div className="col-8 nvx-h-100">
-            <div className="row justify-content-around nvx-h-100">
-
-              <div className="col-6 nvx-h-100 d-flex flex-column justify-content-center align-items-center">
-                <div className="nvx-thumb"></div>
-                <div className="nvx-thumb-text text-center">Tame impala</div>
-              </div>
-              <div className="col-6 nvx-h-100 d-flex flex-column justify-content-center align-items-center">
-                <div className="nvx-thumb"></div>
-                <div className="nvx-thumb-text text-center">Tame impala</div>
-              </div>
-
+            <div className="col-4 nvx-h-100 d-flex flex-column justify-content-center align-items-center">
+              <div className="nvx-thumb"></div>
+              <div className="nvx-thumb-text text-center">Artist name</div>
             </div>
+            <div className="col-4 nvx-h-100 d-flex flex-column justify-content-center align-items-center">
+              <div className="nvx-thumb"></div>
+              <div className="nvx-thumb-text text-center">Artist name</div>
+            </div>
+
           </div>
         </div>
       </div>
 
     </div>
-    <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span className="sr-only">Previous</span>
-    </a>
-    <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-      <span className="carousel-control-next-icon" aria-hidden="true"></span>
-      <span className="sr-only">Next</span>
-    </a>
   </div>
 );
 
--- src/components/Welcome/Slogan.js
+++ src/components/Welcome/Slogan.js
@@ -1,8 +1,19 @@
 //@flow
-import React from 'react';
+import React, { Component } from 'react';
 
-const Slogan = () => (
-  <div className="col nvx-font-size-lg nvx-line-height pt-5">Start jamming now!</div>
-);
+type Props = {}
+
+class Slogan extends Component<Props> {
+
+  constructor(props: Props) {
+
+  }
+
+  render() { 
+    return (
+      <div className="col nvx-font-size-lg nvx-line-height pt-5">Start jamming now!</div>
+    );
+  }
+}
 
 export default Slogan;
