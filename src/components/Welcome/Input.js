0e6e2d494ca6b524015967e2bd07b5b217064be8
ByronGBP <byrongbp@gmail.com>
2018-08-30 15:25:23 +0200

    input styles

--- src/components/Welcome/Input.js
+++ src/components/Welcome/Input.js
@@ -1,11 +1,50 @@
 //@flow
-import React from 'react';
+import React, { Component, createRef } from 'react';
 
-const Input = () => (
-  <div className="col position-relative d-flex">
-    <input type="text" className="nvx-input nvx-font-size-m" name="search" />
-    <label className="nvx-placeholder nvx-font-size-m" htmlFor="search">Search chords for</label>
-  </div>
-);
+import type { ElementRef } from 'react';
+
+type Props = {}
+
+type State = {
+  placeholderClass: string
+}
+
+class Input extends Component<Props, State> {
+
+  inputEl: ?HTMLInputElement;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {placeholderClass: ''};
+  }
+
+  handlePlaceholderClick = () => {
+    this.setState({ placeholderClass: 'nvx-placeholder-active'});
+    if (this.inputEl) {
+      this.inputEl.focus();
+    }
+  }
+
+  handleInputBlur = () => {
+    if (this.inputEl && this.inputEl.value === '') {
+      this.setState({ placeholderClass: ''});
+    }
+  }
+
+  render() {
+    const { placeholderClass } = this.state;
+    return (
+      <div className="col position-relative d-flex">
+        <div className={`py-1 px-3 nvx-placeholder ${placeholderClass}`} onClick={this.handlePlaceholderClick}>
+          <label className="m-0" htmlFor="search">SEARCH CHORDS</label>
+        </div>
+        <input type="text" className="nvx-input nvx-font-size-s" name="search" onBlur={this.handleInputBlur} ref={elem => this.inputEl=elem}/>
+        <div className="d-flex flex-column justify-content-center nvx-search-button">
+          <i className="material-icons">search</i>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Input;
--- src/components/Welcome/Slogan.js
+++ src/components/Welcome/Slogan.js
@@ -2,7 +2,7 @@
 import React from 'react';
 
 const Slogan = () => (
-  <div className="col nvx-font-size-lg nvx-line-height">Start jamming now!</div>
+  <div className="col nvx-font-size-lg nvx-line-height pt-5">Start jamming now!</div>
 );
 
 export default Slogan;
